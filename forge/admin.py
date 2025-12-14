c5941dd1a6411332b86c530d58db17c19dfaa876
Mark <staslive2014@gmail.com>
2025-12-14 12:26:33 +0300

    v5.0 Заполнение БД

--- forge/admin.py
+++ forge/admin.py
@@ -8,3 +8,23 @@ from forge import models
 @admin.register(models.GasStation)
 class GasStation(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.Vehicle)
+class Vehicle(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Refueling)
+class Refueling(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.FuelPrice)
+class FuelPrice(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.FuelStatistics)
+class FuelStatistics(admin.ModelAdmin):
+    pass
