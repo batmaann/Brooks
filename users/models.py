ed9b5217584808c2414b538a15539fdb2fcd668e
Mark <staslive2014@gmail.com>
2026-05-19 20:59:09 +0300

    v2.0 Добавление модели для юзеров

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+
+    middle_name = models.CharField(max_length=150, blank=True, verbose_name="Отчество")
+    phone = models.CharField(max_length=20, blank=True, verbose_name="Телефон")
+
+    def __str__(self):
+        return f"Профиль для {self.user.username}"
