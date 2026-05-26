84e2b72ce5a57fe374857296953dcd02ed1da62a
Mark <staslive2014@gmail.com>
2026-05-26 15:46:18 +0300

    №9 v4.0 тест по заправке

--- tests/refueling/test_refueling.py
+++ tests/refueling/test_refueling.py
@@ -1,4 +1,70 @@
-#TODO
+from decimal import Decimal
+
+import pytest
+from django.apps import apps
+from django.contrib.auth.models import Permission
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework.test import APIClient
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def user(django_user_model):
+    user = django_user_model.objects.create_user(username="testuser", password="pass11111111")
+    perms = Permission.objects.filter(content_type__app_label='forge', content_type__model='refueling')
+    user.user_permissions.set(perms)
+    return user
+
+
+@pytest.fixture
+def api_client(user):
+    client = APIClient()
+    client.force_authenticate(user=user)
+    return client
+
+
+@pytest.fixture
+def vehicle(user):
+    Vehicle = apps.get_model("forge", "Vehicle")
+    return Vehicle.objects.create(
+        name="Тестовый автомобиль (Toyota Camry)",
+        user=user,
+    )
+
+
+# @pytest.fixture
+# def refueling(user, vehicle):
+#     Refueling = apps.get_model("forge", "Refueling")
+#
+#     return Refueling.objects.create(
+#         vehicle=vehicle,
+#         date=timezone.now().date(),
+#         mileage=100,
+#         fuel_quantity=Decimal("45.5"),
+#         price_per_liter=Decimal("55.30"),
+#         user=user,
+#     )
+
+
+def test_create_refueling(api_client, vehicle, user):
+    url = reverse("refueling-list")
+
+    payload = {
+        "vehicle": vehicle.id,
+        "date": timezone.now().date().isoformat(),
+        "mileage": 100,
+        "fuel_quantity": "45.50",
+        "price_per_liter": "55.30",
+    }
+
+    response = api_client.post(url, data=payload, format="json")
+    assert response.status_code == 201
+    assert isinstance(response.data, dict)
+    assert "id" in response.data
+
+# TODO
 # Автоподстановка пользователя (user):
 # Проверить, что если user не указан при сохранении,
 # он автоматически берется из связанного автомобиля (vehicle.user).
@@ -34,4 +100,3 @@
 #
 #
 #
-
