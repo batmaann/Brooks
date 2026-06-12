8869029ae6d4f7bc18f5582d54da3942da85d8eb
Mark <staslive2014@gmail.com>
2026-06-12 16:50:02 +0300

    №35 v3.0 Фикс настроек от бага с пагинацией

--- Brooks/settings.py
+++ Brooks/settings.py
@@ -104,8 +104,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # DRF + drf-spectacular
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
 
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
